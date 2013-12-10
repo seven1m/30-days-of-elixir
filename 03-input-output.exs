fede55bbbef45e79991551b4389fb86c54ddabe3
Tim Morgan <tim@timmorgan.org>
2013-12-09 23:23:00 -0600

    Input/Output is easy!

---
+++ 03-input-output.exs
@@ -0,0 +1,47 @@
+# http://elixir-lang.org/docs/stable/IO.html
+# http://elixir-lang.org/docs/stable/File.html
+
+defmodule CowInterrogator do
+
+  def get_name do
+    String.strip IO.gets("What is your name? ")
+  end
+
+  def get_cow_lover do
+    IO.getn("Do you like cows? ", 1)
+  end
+
+  def interrogate do
+    name = get_name
+    case String.downcase(get_cow_lover) do
+      "y" ->
+        IO.puts "Great! Here's a cow for you #{name}:"
+        IO.puts cow_art
+      "n" ->
+        IO.puts "That's a shame, #{name}."
+      _ ->
+        IO.puts "You should have entered 'Y' or 'N'."
+    end
+  end
+
+  def cow_art do
+    path = Path.expand("../support/cow.txt", __FILE__)
+    case File.read(path) do
+      {:ok, art} -> art
+      {:error, _} -> IO.puts "Error: cow.txt file not found"; System.halt(1)
+    end
+  end
+end
+
+ExUnit.start
+
+defmodule InputOutputTest do
+  use ExUnit.Case
+
+  test "read file" do
+    art = CowInterrogator.cow_art
+    assert String.first(String.strip(art)) == "("
+  end
+end
+
+CowInterrogator.interrogate
