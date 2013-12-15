fb20d1ca55592b8eea055a6cde344ab6f135af76
Tim Morgan <tim@timmorgan.org>
2013-12-15 10:57:24 -0600

    Eliminate a module and just spawn the same one two times.
    Code reuse FTW.

--- 08-process-chat.exs
+++ 08-process-chat.exs
@@ -2,10 +2,10 @@
 # http://benjamintanweihao.github.io/blog/2013/07/13/elixir-for-the-lazy-impatient-and-busy-part-4-processes-101/
 
 # The goal here was to learn about Elixir's (Erlang's) lightweight processes.
-# The following two modules get spawned as processes and start ping/ponging to each other.
+# The following module gets spawned a couple times and ping/pong back and forth.
 
-defmodule Foo do
-  def foo do
+defmodule Pinger do
+  def ping(echo) do
     receive do
       :ok -> :ok
       {sender, _, count} when count > 5 ->
@@ -13,31 +13,16 @@ defmodule Foo do
       {sender, msg, count} ->
         IO.puts "Foo Received: #{inspect msg} (count #{count})"
         :timer.sleep(1000)
-        sender <- {self, "ping", count+1}
-        foo
-    end
-  end
-end
-
-defmodule Bar do
-  def bar do
-    receive do
-      :ok -> :ok
-      {sender, _, count} when count > 5 ->
-        sender <- :ok
-      {sender, msg, count} ->
-        IO.puts "Bar Received: #{inspect msg} (count #{count})"
-        :timer.sleep(1000)
-        sender <- {self, "pong", count+1}
-        bar
+        sender <- {self, echo, count+1}
+        ping(echo)
     end
   end
 end
 
 defmodule Spawner do
   def start do
-    {foo, _foo_monitor} = Process.spawn_monitor(Foo, :foo, [])
-    {bar, _bar_monitor} = Process.spawn_monitor(Bar, :bar, [])
+    {foo, _foo_monitor} = Process.spawn_monitor(Pinger, :ping, ["ping"])
+    {bar, _bar_monitor} = Process.spawn_monitor(Pinger, :ping, ["pong"])
     foo <- {bar, "start", 0}
     wait [foo, bar]
   end
