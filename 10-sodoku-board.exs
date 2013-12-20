74642090fdd32bcccc3d6a1d00915ac9bba65c39
Tim Morgan <tim@timmorgan.org>
2013-12-19 23:03:53 -0600

    Add Sodoku Board.
    For now, it only knows when it's solved. Will be adding a solver soon.

---
+++ 10-sodoku-board.exs
@@ -0,0 +1,58 @@
+defmodule SodokuBoard do
+  @moduledoc "Functions to interrogate a board."
+
+  import Enum
+
+  @doc "Returns true if the board is solved."
+  def solved?(board) do
+    rows_solved?(board) and cols_solved?(board)
+  end
+
+  @doc "Returns true if all rows are solved."
+  def rows_solved?([row | rest]) do
+    max = count(row)
+    sort(row) == to_list(1..max) and rows_solved?(rest)
+  end
+  def rows_solved?([]), do: true
+
+  @doc "Returns true if all columns are solved."
+  def cols_solved?(board) do
+    _cols_solved?(board, count(board)-1)
+  end
+
+  defp _cols_solved?(board, index) when index >= 0 do
+    max = count(board)
+    col = map board, fn row -> at(row, index) end
+    sort(col) == to_list(1..max) and _cols_solved?(board, index-1)
+  end
+  defp _cols_solved?(board, -1), do: true
+end
+
+ExUnit.start
+
+defmodule SodokuBoardTest do
+  use ExUnit.Case
+
+  import SodokuBoard
+
+  test "solved? on solved board" do
+    board = [
+      [1, 2],
+      [2, 1]
+    ]
+    assert solved?(board)
+  end
+
+  test "solved? on incorrectly solved board" do
+    board = [
+      [1, 1],
+      [2, 1]
+    ]
+    assert not solved?(board)
+    board = [
+      [2, 1],
+      [2, 1]
+    ]
+    assert not solved?(board)
+  end
+end
