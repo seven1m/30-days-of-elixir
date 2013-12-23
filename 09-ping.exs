3c0fa42aaee897ab70115bd739f2df26337eadb8
Tim Morgan <tim@timmorgan.org>
2013-12-22 22:37:23 -0600

    Tweak doc strings and minor code reformat.

--- 09-ping.exs
+++ 09-ping.exs
@@ -1,15 +1,21 @@
-# Ping an IP or class-C subnet to find hosts that respond.
-# To run from the command line:
-#
-#     elixir 09-ping.exs 192.168.1.x
-
 defmodule Ping do
-  @doc "Ping an IP asynchronously and send the {ip, exists} tuple to the parent"
+  @moduledoc """
+  Ping a class-C subnet to find hosts that respond.
+  To run from the command line:
+
+    $ elixir 09-ping.exs 192.168.1.x
+  """
+
+  @doc """
+  Ping an IP asynchronously and send the {ip, exists} tuple to the parent
+  """
   def ping_async(ip, parent) do
     parent <- {ip, ping(ip)}
   end
 
-  @doc "Ping a single IP address and return true if there is a response."
+  @doc """
+  Ping a single IP address and return true if there is a response."
+  """
   def ping(ip) do
     result = :os.cmd('ping -c 1 -w 5 -s 1 #{ip}')
     not Regex.match?(%r/100(\.0)?% packet loss/, result)
@@ -17,7 +23,9 @@ defmodule Ping do
 end
 
 defmodule Subnet do
-  @doc "Ping all IPs in a class-C subnet and return a Dict with results."
+  @doc """
+  Ping all IPs in a class-C subnet and return a Dict with results.
+  """
   def ping(subnet) do
     all = ips(subnet)
     Enum.each all, fn ip ->
@@ -26,16 +34,15 @@ defmodule Subnet do
     _wait HashDict.new, Enum.count(all)
   end
 
-  @doc "Given a class-C subnet string like '192.168.1.x', return list of all 254 IPs therein."
+  @doc """
+  Given a class-C subnet string like '192.168.1.x', return list of all 254 IPs therein.
+  """
   def ips(subnet) do
     subnet = Regex.run(%r/^\d+\.\d+\.\d+\./, subnet) |> Enum.at(0)
     Enum.to_list(1..254) |> Enum.map fn i -> "#{subnet}#{i}" end
   end
 
-  defp _wait(dict, 0) do
-    dict
-  end
-
+  defp _wait(dict, 0), do: dict
   defp _wait(dict, remaining) do
     receive do
       {ip, exists} ->
