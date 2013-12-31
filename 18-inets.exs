0797a5eecd101a84435d3a5566db1bc520aec566
Tim Morgan <tim@timmorgan.org>
2013-12-31 17:24:01 -0600

    Serve some dynamic code using built-in inets lib.

--- 18-inets.exs
+++ 18-inets.exs
@@ -3,14 +3,21 @@
 defmodule WebServer do
   def run do
     :inets.start()
-    {:ok, pid} = :inets.start :httpd, [
-      port: 3000,
-      server_name: 'httpd_test',
-      server_root: '/tmp',
-      document_root: '/home/tim',
-      bind_address: {127, 0, 0, 1}
-    ]
+    options = [server_name: 'foo', server_root: '/tmp', document_root: '/tmp', port: 3000, modules: [App]]
+    {:ok, _pid} = :inets.start :httpd, options
     receive do: (_ -> :ok) # TODO better way to wait?
+  end
+end
+
+defmodule App do
+  defrecord :mod, Record.extract(:mod, from_lib: "inets/include/httpd.hrl")
+
+  def unquote(:do)(data) do
+    response = case data.request_uri do
+      '/' -> 'hello world'
+      _   -> ['hello ', data.request_uri]
+    end
+    {:proceed, [response: {200, response}]}
   end
 end
 
