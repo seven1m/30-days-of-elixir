b06948c698039131bcdea888a2c3efc54ae313e0
Tim Morgan <tim@timmorgan.org>
2014-01-14 18:32:21 -0600

    HashSet.

---
+++ 25-set.exs
@@ -0,0 +1,24 @@
+ExUnit.start
+
+defmodule SetTest do
+  use ExUnit.Case
+
+  test "to_list" do
+    list = HashSet.new([1, 2, 2, 3]) |> HashSet.to_list
+    assert list == [1, 2, 3]
+  end
+
+  test "union" do
+    union = HashSet.new([1, 2, 3]) |> HashSet.union(HashSet.new([2, 3, 4]))
+    assert Set.to_list(union) == [1, 2, 3, 4]
+  end
+
+  test "intersection" do
+    union = HashSet.new([1, 2, 3]) |> HashSet.intersection(HashSet.new([2, 3, 4]))
+    assert Set.to_list(union) == [2, 3]
+  end
+
+  test "member?" do
+    refute HashSet.new([1, 3, 5]) |> HashSet.member?(2)
+  end
+end
