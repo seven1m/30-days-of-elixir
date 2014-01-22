c89eef93ae506f55388f79d4cbbacb38b0dd8bf3
Tim Morgan <tim@timmorgan.org>
2014-01-21 22:18:04 -0600

    A slighty better version of Frank. Still not quite there yet!

---
+++ 27-frank-2.exs
@@ -0,0 +1,105 @@
+# A micro web DSL library called "Frank"
+# The motivation was mainly to learn about macros.
+#
+# This version is only slightly better than the last...
+# It still does not support paths with regex match :-(
+#
+# Improvements over the last version are:
+# * define methods as `def handle(path, data)` instead of `def path(data)`
+# * used @before_compile callback to set a default handler method
+
+defmodule Frank do
+  @moduledoc """
+    Frank is a micro web library that provides a DSL for defining routes.
+
+        defmodule MyApp do
+          use Frank
+
+          get "/foo" do
+            response 200, "foo!"
+          end
+        end
+
+        Frank.sing(MyApp)
+  """
+
+  defrecord :mod, Record.extract(:mod, from_lib: "inets/include/httpd.hrl")
+
+  @doc """
+    Start the web server given the 
+  """
+  def sing(module) do
+    :inets.start()
+    options = [server_name: 'frank', server_root: '/tmp', document_root: '/tmp', port: 3000, modules: [module]]
+    {:ok, _pid} = :inets.start :httpd, options
+    IO.puts "running on port 3000"
+  end
+
+  defmodule Path do
+    defmacro get(path, contents) do
+      contents = Macro.escape(Keyword.get(contents, :do))
+      quote bind_quoted: binding do
+        def handle(unquote(path), data) do
+          unquote(contents)
+        end
+      end
+    end
+
+    defmacro __before_compile__(_env) do
+      quote do
+        def handle(_, data) do
+          IO.puts 'here default!'
+          response(404, 'not defined')
+        end
+      end
+    end
+
+    def redirect(path, code // 302) do
+      body = ['redirecting you to <a href="', path, '">', path, '</a>']
+      response code, body, [location: path]
+    end
+
+    def sanitize(content) do
+      content = Regex.replace(%r/&/, content, "\\&amp;")
+      content = Regex.replace(%r/</, content, "\\&lt;")
+      content = Regex.replace(%r/>/, content, "\\&gt;")
+      content
+    end
+
+    def response(code, body, headers // []) do
+      if is_binary(body) do
+        body = bitstring_to_list(body)
+      end
+      headers = [code: code, content_length: integer_to_list(iolist_size(body))] ++ headers
+      {:proceed, [response: {:response, headers, body}]}
+    end
+  end
+
+  defmacro __using__(_opts) do
+    quote do
+      import Frank.Path
+
+      def unquote(:do)(data) do
+        name = data.request_uri
+        IO.puts "calling #{name}"
+        handle(list_to_bitstring(name), data)
+      end
+
+      @before_compile Path
+    end
+  end
+end
+
+defmodule Test do
+  use Frank
+
+  get "/" do
+    response 200, "<a href='/foo'>go to foo</a>"
+  end
+
+  get "/foo" do
+    response 200, "foo!"
+  end
+end
+
+Frank.sing(Test)
