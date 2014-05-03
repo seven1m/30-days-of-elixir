fc31d433436fb1f8052fdcfafbc442674e6d7be3
Tim Morgan <tim@timmorgan.org>
2014-05-02 23:44:00 -0500

    Fix syntax and use of char lists with Regex.

--- 21-wiki.exs
+++ 21-wiki.exs
@@ -1,4 +1,5 @@
 require IEx
+require Record
 
 defmodule Wiki do
   @moduledoc """
@@ -8,7 +9,7 @@ defmodule Wiki do
 
     To run:
 
-    $ iex 20-wiki.exs
+    $ iex 21-wiki.exs
 
     ... then point your browser to http://localhost:3000
   """
@@ -16,8 +17,8 @@ defmodule Wiki do
   defrecord :mod, Record.extract(:mod, from_lib: "inets/include/httpd.hrl")
 
   @page_name "([A-Z][a-z0-9]+){2,}"
-  @valid_page_name %r/^#{@page_name}$/
-  @edit_path %r/^#{@page_name}\/edit$/
+  @valid_page_name ~r/^([A-Z][a-z0-9]+){2,}$/
+  @edit_path ~r/^([A-Z][a-z0-9]+){2,}\/edit$/
 
   @doc """
     Start the inets server.
@@ -35,25 +36,25 @@ defmodule Wiki do
     cond do
       name == '' ->
         redirect('/HomePage')
-      Regex.match?(@valid_page_name, name) ->
+      Regex.match?(@valid_page_name, list_to_bitstring(name)) ->
         case data.method do
           'GET'  -> render_page(name)
           'POST' -> save_page(name, data); redirect(name)
         end
-      Regex.match?(@edit_path, name) ->
-        name = Regex.replace(%r/\/edit$/, name, "")
+      Regex.match?(@edit_path, list_to_bitstring(name)) ->
+        name = Regex.replace(~r/\/edit$/, list_to_bitstring(name), "")
         render_page(name, :edit)
       true ->
         response(404, 'bad path')
     end
   end
 
-  def redirect(path, code // 302) do
+  def redirect(path, code \\ 302) do
     body = ['redirecting you to <a href="', path, '">', path, '</a>']
     response code, body, [location: path]
   end
 
-  def render_page(name, action // :show) do
+  def render_page(name, action \\ :show) do
     case {action, File.read(page_path(name))} do
       {:edit, {:ok, body}} ->
         response 200, edit_page_form(name, body) |> bitstring_to_list
@@ -73,14 +74,14 @@ defmodule Wiki do
   end
 
   def sanitize(content) do
-    content = Regex.replace(%r/&/, content, "\\&amp;")
-    content = Regex.replace(%r/</, content, "\\&lt;")
-    content = Regex.replace(%r/>/, content, "\\&gt;")
+    content = Regex.replace(~r/&/, content, "\\&amp;")
+    content = Regex.replace(~r/</, content, "\\&lt;")
+    content = Regex.replace(~r/>/, content, "\\&gt;")
     content
   end
 
   def breakify(content) do
-    Regex.replace(%r/\r?\n/, content, "<br>")
+    Regex.replace(~r/\r?\n/, content, "<br>")
   end
 
   def linkify(content) do
@@ -105,11 +106,11 @@ defmodule Wiki do
     File.write!(page_path(name), content)
   end
 
-  def edit_page_form(name, content // "") do
+  def edit_page_form(name, content \\ "") do
     "<form action='/#{name}' method='post'><textarea name='content' rows='25' cols='80'>#{content}</textarea><br><button>Save</button><a href='/#{name}'>cancel</a></form>"
   end
 
-  def response(code, body, headers // []) do
+  def response(code, body, headers \\ []) do
     headers = [code: code, content_length: integer_to_list(iolist_size(body))] ++ headers
     {:proceed, [response: {:response, headers, body}]}
   end
