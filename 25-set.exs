370d14a5e90eec24ba7e9740387063cc01899084
Tim Morgan <tim@timmorgan.org>
2014-05-02 23:51:59 -0500

    Ugg, not in love with all this Enum.into stuff.

--- 25-set.exs
+++ 25-set.exs
@@ -4,21 +4,21 @@ defmodule SetTest do
   use ExUnit.Case
 
   test "to_list" do
-    list = HashSet.new([1, 2, 2, 3]) |> HashSet.to_list
+    list = Enum.into([1, 2, 2, 3], HashSet.new) |> HashSet.to_list
     assert list == [2, 3, 1] # unintuitive ordering
   end
 
   test "union" do
-    union = HashSet.new([1, 2, 3]) |> HashSet.union(HashSet.new([2, 3, 4]))
+    union = Enum.into([1, 2, 3], HashSet.new) |> HashSet.union(Enum.into([2, 3, 4], HashSet.new))
     assert Set.to_list(union) == [2, 3, 4, 1] # unintuitive, but somewhat more understandable
   end
 
   test "intersection" do
-    union = HashSet.new([1, 2, 3]) |> HashSet.intersection(HashSet.new([2, 3, 4]))
+    union = Enum.into([1, 2, 3], HashSet.new) |> HashSet.intersection(Enum.into([2, 3, 4], HashSet.new))
     assert Set.to_list(union) == [2, 3]
   end
 
   test "member?" do
-    refute HashSet.new([1, 3, 5]) |> HashSet.member?(2)
+    refute Enum.into([1, 3, 5], HashSet.new) |> HashSet.member?(2)
   end
 end
