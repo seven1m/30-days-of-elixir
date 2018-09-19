eb36a7a1f75943656ec454abd90afbade325c420
Mark Holmberg <mark.holmberg@icentris.com>
2018-09-19 14:18:12 -0600

    Updates List filter_map to remove deprecated syntax.

--- 04-list.exs
+++ 04-list.exs
@@ -5,31 +5,31 @@ ExUnit.start
 defmodule ListTest do
   use ExUnit.Case
 
-  def sample do
+  def sample() do
     ["Tim", "Jen", "Mac", "Kai"]
   end
 
   test "sigil" do
-    assert sample == ~w(Tim Jen Mac Kai)
+    assert sample() == ~w(Tim Jen Mac Kai)
   end
 
   test "head" do
-    [head | _] = sample
+    [head | _] = sample()
     assert head == "Tim"
   end
 
   test "tail" do
-    [_ | tail] = sample
+    [_ | tail] = sample()
     assert tail == ~w(Jen Mac Kai)
   end
 
   # warning, has to traverse the entire list!
   test "last item" do
-    assert List.last(sample) == "Kai"
+    assert List.last(sample()) == "Kai"
   end
 
   test "delete item" do
-    assert List.delete(sample, "Mac") == ~w(Tim Jen Kai)
+    assert List.delete(sample(), "Mac") == ~w(Tim Jen Kai)
     # only deletes the first occurrence
     assert List.delete([1, 2, 2, 3], 2) == [1, 2, 3]
   end
@@ -54,19 +54,21 @@ defmodule ListTest do
 
   # List.wrap is much like Ruby's Array() method
   test "wrap" do
-    assert List.wrap(sample) == sample
+    assert List.wrap(sample()) == sample()
     assert List.wrap(1) == [1]
     assert List.wrap([]) == []
     assert List.wrap(nil) == []
   end
 
+  # https://niallburkley.com/blog/elixir-filter-map/
   test "Enum.filter_map" do
-    some = Enum.filter_map sample, &(String.first(&1) >= "M"), &(&1 <> " Morgan")
+    # some = Enum.filter_map sample(), &(String.first(&1) >= "M"), &(&1 <> " Morgan")
+    some = sample() |> Enum.filter(fn(value) -> String.first(value) >= "M" end) |> Enum.map(fn(filtered_value) -> filtered_value <> " Morgan" end)
     assert some == ["Tim Morgan", "Mac Morgan"]
   end
 
   test "list comprehension" do
-    some = for n <- sample, String.first(n) < "M", do: n <> " Morgan"
+    some = for n <- sample(), String.first(n) < "M", do: n <> " Morgan"
     assert some == ["Jen Morgan", "Kai Morgan"]
   end
 
