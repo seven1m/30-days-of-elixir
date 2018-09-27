9e83127a9ca07f71f935116a7dd099729b5e6083
Fábio Beirão <fdbeirao@gmail.com>
2018-09-27 17:06:13 +0200

    Fix deprecated syntax to remove compilation warnings

--- 05-map.exs
+++ 05-map.exs
@@ -10,45 +10,45 @@ defmodule MapTest do
   end
 
   test "Map.get" do
-    assert Map.get(sample, :foo) == 'bar'
-    assert Map.get(sample, :non_existent) == nil
+    assert Map.get(sample(), :foo) == 'bar'
+    assert Map.get(sample(), :non_existent) == nil
   end
 
   test "[]" do
-    assert sample[:foo] == 'bar'
-    assert sample[:non_existent] == nil
+    assert sample()[:foo] == 'bar'
+    assert sample()[:non_existent] == nil
   end
 
   test "." do
-    assert sample.foo == 'bar'
+    assert sample().foo == 'bar'
     assert_raise KeyError, fn ->
-      sample.non_existent
+      sample().non_existent
     end
   end
 
   test "Map.fetch" do
-    {:ok, val} = Map.fetch(sample, :foo)
+    {:ok, val} = Map.fetch(sample(), :foo)
     assert val == 'bar'
-    :error = Map.fetch(sample, :non_existent)
+    :error = Map.fetch(sample(), :non_existent)
   end
 
   test "Map.put" do
-    assert Map.put(sample, :foo, 'bob') == %{foo: 'bob', baz: 'quz'}
-    assert Map.put(sample, :far, 'bar') == %{foo: 'bar', baz: 'quz', far: 'bar'}
+    assert Map.put(sample(), :foo, 'bob') == %{foo: 'bob', baz: 'quz'}
+    assert Map.put(sample(), :far, 'bar') == %{foo: 'bar', baz: 'quz', far: 'bar'}
   end
 
   test "Update map using pattern matching syntax" do
     # You can only update existing keys in this way
-    assert %{sample | foo: 'bob'} == %{foo: 'bob', baz: 'quz'}
+    assert %{ sample() | foo: 'bob'} == %{foo: 'bob', baz: 'quz'}
     # It doesn't work if you want to add new keys
     assert_raise KeyError, fn ->
-      %{sample | far: 'bob'}
+      %{ sample() | far: 'bob'}
     end
   end
 
   test "Map.values" do
     # Map does not preserve order of keys, thus we Enum.sort
-    assert Enum.sort(Map.values(sample)) == ['bar', 'quz']
+    assert Enum.sort(Map.values(sample())) == ['bar', 'quz']
   end
 end
 
