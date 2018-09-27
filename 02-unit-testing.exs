c5ad75bf24c4d64da1705f13f50c0a020b33504d
Fábio Beirão <fdbeirao@users.noreply.github.com>
2018-09-27 15:52:28 +0200

    Update ExUnit documentation link
    The old link is now returning a 404.

--- 02-unit-testing.exs
+++ 02-unit-testing.exs
@@ -1,5 +1,5 @@
 # http://elixir-lang.org/getting-started/mix-otp/introduction-to-mix.html
-# http://elixir-lang.org/docs/stable/ex_unit/ExUnit.html
+# https://hexdocs.pm/ex_unit/ExUnit.html
 
 ExUnit.start                                # set up the test runner
 
