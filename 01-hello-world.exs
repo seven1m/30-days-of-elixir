a2de5f4c39313f767708aad402a0802d201247da
Anatolij <developer@languages-with-music.com>
2019-05-20 08:50:16 +0200

    How to run it
    add a description how to run it

--- 01-hello-world.exs
+++ 01-hello-world.exs
@@ -1,3 +1,11 @@
 # http://elixir-lang.org/getting-started/introduction.html
 
 IO.puts "hello world"
+
+# How to run it
+#
+# in a command line application in a file directory do this command(-s):
+#
+# elixir 01-hello-world.exs
+# elixirc 01-hello-world.exs
+# iex 01-hello-world.exs
